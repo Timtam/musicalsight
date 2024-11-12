075057330c8364616360f8c69950756abedc790e
Toni Barth <software@satoprogs.de>
2024-11-12 19:39:08 +0100

    update and new product

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import { HashRouter } from "react-router-dom"
 import "reflect-metadata"
 import App from "./App"
 import "./index.css"
-import reportWebVitals from "./reportWebVitals"
 
 const container = document.getElementById("root")
 const root = createRoot(container!)
@@ -16,8 +15,3 @@ root.render(
         </HashRouter>
     </React.StrictMode>,
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
