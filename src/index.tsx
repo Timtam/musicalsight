41e8fbd3755acfb2a105094091bd45a7b78b378d
Toni Barth <software@satoprogs.de>
2025-02-05 16:35:11 +0100

    remove i and update react-helmet to react-helmet-async

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { HelmetProvider } from "react-helmet-async"
 import { RouterProvider, createHashRouter } from "react-router-dom"
 import "reflect-metadata"
 import App from "./App"
@@ -79,6 +80,8 @@ const router = createHashRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <HelmetProvider>
+            <RouterProvider router={router} />
+        </HelmetProvider>
     </React.StrictMode>,
 )
