a303a2a81005ee496d9654cc0706fb21848a26f1
Toni Barth <contact@toni-barth.online>
2025-10-31 01:50:31 +0100

    merge in the new reaper accessible plugin catalog

--- scripts/import-reaperaccessible.ts
+++ scripts/import-reaperaccessible.ts
@@ -1,33 +1,216 @@
 import toml from "@iarna/toml"
-import * as cheerio from "cheerio"
+import { parse } from "csv-parse/sync"
 import fs from "fs/promises"
 import fetch from "node-fetch"
 import slugify from "slugify"
 
 const downloadSource = async () => {
     let res = await fetch(
-        "https://reaperaccessible.fr/archives/Native%20Instruments%20Kontakt%20libraries%20catalog.html",
+        "https://reaperaccessible.fr/catalog/data/catalog.csv",
     )
-    const buffer = await res.arrayBuffer()
-    const decoder = new TextDecoder("iso-8859-1")
-    return decoder.decode(buffer)
+    return await res.text()
 }
 
-const nextElement = (
-    $: ReturnType<cheerio.load>,
-    $elem: cheerio.AnyNode,
-    filter?: RegExp,
-): cheerio.AnyNode => {
-    while (
-        ($($elem).next().length > 0 &&
-            $($($elem).next()).text().trim() === "") ||
-        (filter && filter.test($($($elem).next()).text().replaceAll("\n", " ")))
-    ) {
-        $elem = $($elem).next()
+const parseSize = (s: string): float | undefined => {
+    if (s === "") return
+
+    let size: number | undefined
+
+    let match = s.match(/(\d+,?\.?\d*) ?(MB|GB)?/i)
+
+    if (match !== null) {
+        size = parseFloat(match[1].replace(",", "."))
     }
-    if ($($elem).next().length === 0) return null
-    return $($elem).next()
+
+    if (size === undefined && !["No"].includes(s)) {
+        throw new Error(`Unable to parse size from ${s}`)
+    }
+
+    if (s.endsWith("GB")) {
+        size *= 1000
+    }
+
+    return size
 }
+
+const parseRequirement = (
+    req: string,
+    v: string,
+): string | string[] | undefined => {
+    if (req === "") return
+
+    let requirement: string = ""
+    let version: string = ""
+
+    switch (req) {
+        case "Kontakt Full":
+            requirement = "native-instruments-kontakt"
+            break
+        case "Kontakt Player":
+            requirement = "native-instruments-kontakt-player"
+            break
+        case "Sforzando":
+            requirement = "plogue-sforzando"
+            break
+        default:
+            throw new Error(`unknown requirement ${req}`)
+    }
+
+    if (v !== "") {
+        version = v
+    }
+
+    if (version === "" && requirement !== "") return requirement
+    else if (version !== "" && requirement !== "") return [requirement, version]
+    throw new Error(`invalid requirements ${r} with version ${v} supplied`)
+}
+
+/*
+const parseCategories = (
+    type: string,
+    itype: string,
+    etype: string,
+): string[] => {
+    switch (type) {
+        case "Instrument":
+            switch (itype.trim().toLowerCase()) {
+                case "electric piano":
+                case "hybrid piano":
+                case "symphonic strings":
+                case "double bass ensemble":
+                case "solo violin":
+                case "keys":
+                case "keyboard":
+                case "organ":
+                case "tape keyboard":
+                case "piano":
+                case "toy piano":
+                    return ["vsti.keys"]
+                case "string ensemble":
+                case "strings":
+                case "violin":
+                case "viola":
+                case "cello":
+                case "double bass":
+                case "harp":
+                    return ["vsti.strings"]
+                case "preset browser":
+                case "workstation":
+                case "synth":
+                    return ["vsti.synthesizer"]
+                case "percussion":
+                case "mallets":
+                case "percussion ensemble":
+                case "rhythm/percussion":
+                    return ["vsti.percussion"]
+                case "orchestra":
+                case "orchestral effects":
+                    return ["vsti.brass", "vsti.strings", "vsti.woodwinds"]
+                case "bassoon":
+                case "clarinet":
+                case "flute":
+                case "oboe":
+                case "woodwind ensemble":
+                case "woodwinds":
+                case "alto saxophone":
+                case "tenor saxophone":
+                case "soprano saxophone":
+                case "baritone saxophone":
+                case "bass clarinet":
+                case "english horn":
+                case "contrabassoon":
+                    return ["vsti.woodwinds"]
+                case "world instruments":
+                case "banjo":
+                case "didgeridoo":
+                case "dhol ensemble":
+                case "xiao flute":
+                case "dobro":
+                case "dulcimer":
+                case "guitar dobro":
+                case "guzheng":
+                case "chinese lute":
+                case "transverse flute":
+                case "vertical bamboo flute":
+                case "transverse bamboo flute":
+                case "erhu":
+                case "guqin":
+                case "konghou (chinese harp)":
+                    return ["vsti.ethnic"]
+                case "chinese percussion ensemble":
+                    return ["vsti.ethnic", "vsti.percussion"]
+                case "producer tool":
+                    return ["vsti.drums", "vsti.synthesizer"]
+                case "vocal":
+                case "choir":
+                case "vocal phrases":
+                    return ["vsti.vocal"]
+                case "cinematic effects":
+                case "effects/sound design":
+                case "hybrid sound design":
+                case "sound design":
+                    return ["vsti.sound-design"]
+                case "guitar ambience":
+                    return ["vsti.ambient"]
+                case "brass":
+                case "trumpet":
+                case "flugelhorn":
+                case "french horn":
+                case "euphonium":
+                case "tuba":
+                case "trombone":
+                case "brass ensemble":
+                    return ["vsti.brass"]
+                case "drum kit":
+                case "tom ensemble/percussion":
+                case "steel tongue drum":
+                case "drum synth":
+                    return ["vsti.drums"]
+                case "sampler":
+                    return ["vsti.sampler"]
+                case "groove workstation":
+                    return [
+                        "vsti.drums",
+                        "vsti.percussion",
+                        "vsti.synthesizer",
+                        "vsti.sound-design",
+                    ]
+                case "12-string acoustic guitar":
+                case "acoustic bass":
+                case "electric bass (5-string)":
+                case "electric bass (6-string)":
+                case "acoustic guitar":
+                case "classical guitar":
+                case "electric bass":
+                case "electric bass (fretless)":
+                case "electric guitar":
+                case "guitar":
+                case "jumbo acoustic guitar":
+                case "slide guitar":
+                case "ukulele":
+                case "upright bass":
+                    return ["vsti.guitar"]
+                case "instruments bundle":
+                case "instrument collection":
+                case "":
+                    return ["vsti"]
+                default:
+                    throw new Error(`Unknown instrument type ${itype}`)
+            }
+        case "Effect":
+            switch (etype) {
+                case "":
+                    return ["fx"]
+                default:
+                    throw new Error(`Unknown effect type ${etype}`)
+            }
+        case "":
+            return
+        default:
+            throw new Error(`Unknown product type ${type}`)
+    }
+}
+*/
 
 const main = async () => {
     let vendors: string
@@ -47,220 +230,97 @@ const main = async () => {
     let oProducts: Record<string, any> = toml.parse(products)
     let oVendors: Record<string, any> = toml.parse(vendors)
 
-    const html = await downloadSource()
-    const $ = cheerio.load(html)
+    const csv = await downloadSource()
+    const raData = parse(csv, {
+        delimiter: ",",
+        columns: true,
+        skip_empty_lines: true,
+        bom: true,
+        record_delimiter: "\r\n",
+    })
 
-    const $vendors = $("h1")
+    let line = 1
 
-    for (const $v of $vendors) {
-        const vId = slugify($($v).text(), {
-            lower: true,
-        })
-        if (!oVendors[vId]) {
-            oVendors[vId] = {
-                name: $($v).text().replace("\n", " "),
-                url: "",
+    for (const raProduct of raData) {
+        line += 1
+        try {
+            const vId = slugify(raProduct["Developer"], {
+                lower: true,
+            })
+
+            if (!oVendors[vId]) {
+                oVendors[vId] = {
+                    name: raProduct["Developer"],
+                    url: "",
+                }
             }
-        }
 
-        let $next = $($v).next()
-
-        let product: Record<string, any>
-
-        do {
-            product = {
+            let product: Record<string, any> = {
                 vendor: vId,
             }
 
-            if ($($next).next().get(0).tagName === "h3") {
-                // some products are grouped, we don't need the group names, but the actual product names
-                $next = $($next).next()
-            }
-
-            product["name"] = $($next).text().replaceAll("\n", " ")
-
-            if (
-                product["name"]
-                    .toLowerCase()
-                    .startsWith(oVendors[vId]["name"].toLowerCase() + " - ")
-            ) {
-                product["name"] = product["name"].substring(
-                    oVendors[vId]["name"].length + 3,
-                )
-            }
-
-            if (
-                product["name"]
-                    .toLowerCase()
-                    .endsWith(" - " + oVendors[vId]["name"].toLowerCase())
-            ) {
-                product["name"] = product["name"].substring(
-                    0,
-                    oVendors[vId]["name"].length,
-                )
-            }
+            product["name"] = raProduct["Name"]
 
             const pId = slugify(product["name"], {
                 lower: true,
             })
 
-            $next = nextElement($, $next)
+            product["size"] = parseSize(raProduct["Size"])
 
-            if ($($next).text().startsWith("Size:")) {
-                product["size"] = parseFloat(
-                    $($next)
-                        .text()
-                        .replaceAll("\n", " ")
-                        .match(/Size: ?(\d+,?\.?\d*) ?(mo|go)?/i)[1]
-                        .replace(",", "."),
-                )
+            product["requires"] = [
+                parseRequirement(
+                    raProduct["SamplerType"],
+                    raProduct["SamplerMinVersion"],
+                ),
+            ]
 
-                if ($($next).text().toLowerCase().endsWith("go")) {
-                    product["size"] *= 1000
-                }
-                $next = nextElement($, $next)
-            }
-
-            if ($($next).text().startsWith("Configuration:")) {
-                product["requires"] = [["native-instruments-kontakt"]]
-
-                const version = $($next)
-                    .text()
-                    .replaceAll("\n", " ")
-                    .match(/Configuration: KONTAKT? ?(.*)/i)[1]
-
-                if (version !== "") product["requires"][0].push(version)
-
-                $next = nextElement($, $next)
-            }
-
-            const nks = $($next)
-                .text()
-                .replaceAll("\n", " ")
-                .toLowerCase()
-                .match(/nks-? ?compatible: (yes|no)/)[1]
-
-            if (nks === "yes") {
+            if (raProduct["NKSCompatible"] === "Yes") {
                 product["nks"] = true
-                $next = nextElement($, $next, /nks-? ?compatible: (yes|no)/i)
-            }
-
-            if (nks === "no") {
+            } else if (raProduct["NKSCompatible"] === "No") {
                 product["nks"] = false
-                $next = nextElement($, $next, /nks-? ?compatible: (yes|no)/i)
+            } else if (raProduct["NKSCompatible"] === "ThirdParty") {
+                product["nks"] =
+                    "NKS is provided by third-party products (see links section)"
+                if (raProduct["ThirdpartyNKSLink"])
+                    product["additional_links"] = {
+                        "NKS Snapshots": raProduct["ThirdpartyNKSLink"],
+                    }
             }
 
-            if (
-                $($next)
-                    .text()
-                    .replaceAll("\n", " ")
-                    .toLowerCase()
-                    .startsWith("accessible parameters") ||
-                $($next)
-                    .text()
-                    .replaceAll("\n", " ")
-                    .toLowerCase()
-                    .startsWith("parameters accessible") ||
-                $($next)
-                    .text()
-                    .replaceAll("\n", " ")
-                    .toLowerCase()
-                    .includes("accessibles")
-            ) {
-                $next = nextElement($, $next)
-            }
-
-            if (
-                $($next)
-                    .text()
-                    .replaceAll("\n", " ")
-                    .toLowerCase()
-                    .startsWith("csi zone")
-            ) {
-                $next = nextElement($, $next)
-            }
-
-            if (
-                $($next)
-                    .text()
-                    .replaceAll("\n", " ")
-                    .toLowerCase()
-                    .startsWith("download csi")
-            ) {
-                $next = nextElement($, $next)
-            }
-
-            if (
-                $($next)
-                    .text()
-                    .replaceAll("\n", " ")
-                    .toLowerCase()
-                    .includes("demo here") ||
-                $($next)
-                    .text()
-                    .replaceAll("\n", " ")
-                    .toLowerCase()
-                    .includes("partie 1 ici.")
-            ) {
-                product["demo"] = $($($next).children("a")[0]).attr("href")
-                $next = nextElement(
-                    $,
-                    $next,
-                    /(.*partie \d+ ici.)|(.*demo here.*)/i,
+            if (raProduct["AudioDemoURL"].startsWith("http")) {
+                product["demo"] = raProduct["AudioDemoURL"]
+            } else if (raProduct["AudioDemoURL"] !== "") {
+                product["demo"] = encodeURI(
+                    `https://reaperaccessible.fr/catalog/demos/${raProduct["AudioDemoURL"]}`,
                 )
             }
 
-            if (
-                $($next).text().replaceAll("\n", " ").toLowerCase() ===
-                    "demo not available." ||
-                $($next)
-                    .text()
-                    .replaceAll("\n", " ")
-                    .toLowerCase()
-                    .startsWith("pas de ")
-            ) {
-                $next = nextElement($, $next)
-            }
+            if (raProduct["DescriptionEN"])
+                product["description"] = raProduct["DescriptionEN"]
+            if (raProduct["ProductURL"])
+                product["url"] = raProduct["ProductURL"]
+
+            if (raProduct["Notes"])
+                product["accessibility_description"] = raProduct["Notes"]
+
+            /*
+            product["categories"] = parseCategories(
+                raProduct["Type"],
+                raProduct["InstrumentTypeEN"],
+                raProduct["EffectTypeEN"],
+            )
+*/
 
             if (
-                $($next).text().toLowerCase() === "presentation:" ||
-                $($next).text().toLowerCase() === "description:"
+                !oProducts[pId] ||
+                oProducts[pId]["price"] === undefined ||
+                oProducts[pId]["categories"] === undefined
             ) {
-                product["description"] = ""
-                $next = nextElement($, $next)
-
-                while ($next !== null && $($next).get(0).tagName === "p") {
-                    product["description"] =
-                        product["description"] + "\n" + $($next).text()
-                    $next = nextElement($, $next)
-                }
-            }
-
-            if (
-                ($next !== null &&
-                    $($next).text().toLowerCase() === "comment:") ||
-                $($next).text().toLowerCase().startsWith("commentaire")
-            ) {
-                $next = nextElement($, $next)
-            }
-
-            if (!oProducts[pId]) {
                 oProducts[pId] = product
             }
-        } while (
-            $next !== null &&
-            ["h2", "h3"].includes($($next).get(0).tagName)
-        )
-
-        if ($next !== null && $($next).get(0).tagName !== "h1") {
+        } catch (e) {
             console.log(
-                "Unparsed tag " +
-                    $($next).get(0).tagName +
-                    " found with text " +
-                    $($next).text(),
-            )
-            console.log(
-                "Current product: " + JSON.stringify(product, undefined, 2),
+                `Error for product ${raProduct["Name"]} by vendor ${raProduct["Developer"]} at record ${line}: ${e.message}`,
             )
             return
         }
